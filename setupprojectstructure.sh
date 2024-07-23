4cc73ffc9f4702f748088a228caf9832e1ec3869
rasad4468 <73251163+rasad4468@users.noreply.github.com>
2024-07-23 11:43:09 -0500

    Create setupprojectstructure.sh
    Terraform

---
+++ setupprojectstructure.sh
@@ -0,0 +1,9 @@
+mkdir -p terraform/environments/dev
+mkdir -p terraform/modules/vnet-spoke
+mkdir -p test
+touch terraform/environments/dev/main.tf
+touch terraform/environments/dev/variables.tf
+touch terraform/environments/dev/outputs.tf
+touch terraform/modules/vnet-spoke/main.tf
+touch test/terraform_test.go
+touch README.md
